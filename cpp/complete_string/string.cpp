99e2799817f7e9ecabece7e000359b6b18594f5a
liutao932 <87967759+liutao932@users.noreply.github.com>
2022-03-01 17:58:36 +0800

    Create string.cpp

---
+++ cpp/complete_string/string.cpp
@@ -0,0 +1,12 @@
+#include"string.h"
+//using namespace BitString1;
+using namespace BitString2;
+int main()
+{
+	//StringTest();
+	//StringTest1();
+	//StringTest2();
+	StringTest1();
+	/*std::string s1("hello world");
+	cout << s1.find("hew", 0) << endl;*/
+}
