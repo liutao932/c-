c5cfb554d792e4f5321f7c14616da7e8d09df355
liutao932 <87967759+liutao932@users.noreply.github.com>
2022-05-20 22:09:35 +0800

    Create map.h

---
+++ cpp/map_and_set/map.h
@@ -0,0 +1,53 @@
+#pragma once
+#include "RBTree.h"
+
+namespace bit
+{
+	template < class K, class V>
+	class map
+	{
+	public:
+		struct MapKeyOfT
+		{
+			const K& operator()(const pair<K, V>& kv)
+			{
+				return kv.first;
+			}
+		};
+
+		typedef typename RBTree<K, pair<K, V>, MapKeyOfT>::iterator iterator;
+
+		iterator begin()
+		{
+			return _t.begin();
+		}
+
+		iterator end()
+		{
+			return _t.end();
+		}
+
+		bool insert(const pair<K, V>& kv)
+		{
+			return _t.Insert(kv);
+		}
+	private:
+		RBTree<K, pair<K, V>, MapKeyOfT> _t;
+	};
+
+	void test_map()
+	{
+		map<string, string> dict;
+		dict.insert(make_pair("sort", "排序"));
+		dict.insert(make_pair("string", "字符串"));
+		dict.insert(make_pair("map", "字典"));
+
+		auto it = dict.begin();
+		while (it != dict.end())
+		{
+			cout << it->first << ":" << it->second << endl;
+			++it;
+		}
+		cout << endl;
+	}
+}
