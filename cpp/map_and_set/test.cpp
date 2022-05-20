8a1b97360a08315df35d23999279cfb93f7aa520
liutao932 <87967759+liutao932@users.noreply.github.com>
2022-05-20 22:10:26 +0800

    Create test.cpp

---
+++ cpp/map_and_set/test.cpp
@@ -0,0 +1,27 @@
+#include <set>
+#include <map>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <functional>
+#include <assert.h>
+using namespace std;
+#include"map.h"
+#include "RBTree.h"
+#include"set.h"
+
+int main()
+{
+	//test_set1();
+	//test_multiset();
+	//test_map4();
+	//TestAVLTree();
+	//TestRBTree();
+
+	bit::test_set();
+	bit::test_map();
+
+	return 0;
+}
